017877733114fb3cfe99a9a9533d05fea0546e45
Hukam Singh <48188222+ImHukam@users.noreply.github.com>
2022-10-21 20:45:19 +0530

    Create 721.sol

---
+++ contracts/721.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract DemoErc721 is ERC721URIStorage, ERC721Enumerable{
+
+    string uri = "https://ipfs.io/ipfs/QmQqzMTavQgT4f4T5v6PWBp7XNKtoPmC9jvn12WPT3gkSE";
+    uint256 counter;
+    
+    constructor() ERC721("batchminter", "BAM") {}
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function mint(address to, uint256 quantity) public {
+        for (uint256 i = 0; i < quantity; i++) {
+            _safeMint(to, counter);
+           _setTokenURI(counter, uri);
+           counter= counter + 1;
+        }
+    }
+}
